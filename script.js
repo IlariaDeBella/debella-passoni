3b8730ee1960854b9015c510eb6d2be27678b609
gggiorginaaa <giorgia.passoni@liceobanfi.eu>
2021-05-30 15:54:13 +0200

    aggiunta buy nello shop

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 var n_img = 3;
 var corrente = 1;
-function succ(){
+
+function succ() {
     corrente++;
 
-if(corrente>n_img){
-    corrente = 1;
+    if (corrente > n_img) {
+        corrente = 1;
+    }
+    for (var i = n_img; i > 0; i--) {
+        document.querySelector("#img_slider img:nth-child(" + i + ")").style.display = "none";
+    }
+    document.querySelector("#img_slider img:nth-child(" + corrente + ")").style.display = "block";
 }
-for(var i = n_img; i>0; i--){
-    document.querySelector("#img_slider img:nth-child(" + i + ")").style.display = "none";
-}
-document.querySelector("#img_slider img:nth-child(" + corrente + ")").style.display = "block";
-}
-function prec(){
+
+function prec() {
     corrente--;
-    if(corrente ==0){
+    if (corrente == 0) {
         corrente = n_img;
     }
-    for(var i = n_img; i > 0; i--){
+    for (var i = n_img; i > 0; i--) {
         document.querySelector("#img_slider img:nth-child(" + i + ")").style.display = "none";
     }
     document.querySelector("#img_slider img:nth-child(" + corrente + ")").style.display = "block";
@@ -24,5 +26,25 @@ function prec(){
 }
 setInterval(succ, 5000);
 
+function pop_init(my_content) {
 
+    var pop_height = "25%";
+    var pop_html = "<div class="
+    pop - bg "></div><div class="
+    pop - wrap "><p class="
+    pop - x ">X</p><div class="
+    pop - content "></div></div>";
 
+    $("body").prepend(pop_html);
+
+    $(".pop-wrap").animate({ "height": pop_height }, 250).click(function(event) {
+        event.stopPropagation();
+    });
+
+    $(".pop-bg, .pop-x").bind("click", function() {
+        pop_close();
+    });
+
+    $(".pop-content").text(my_content);
+
+}
